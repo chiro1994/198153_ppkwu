66fadfff51dc166a71bebd23bee9f1c3f08292d0
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2018-04-15 10:15:12 +0200

    Zadanie 2 - dodanie klasy EmailValidator

---
+++ validator/src/main/java/com/example/validator/validators/EmailValidator.java
@@ -0,0 +1,20 @@
+package com.example.validator.validators;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailValidator{
+    private static Pattern pattern;
+    private static Matcher matcher;
+    private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    public static boolean isEmailValid(String username) {
+        return (validateEmail(username));
+    }
+
+    private static boolean validateEmail(final String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+}
