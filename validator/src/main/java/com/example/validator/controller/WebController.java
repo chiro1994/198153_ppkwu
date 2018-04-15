55b5c5202fea268beba10ac438b820569abed57f
Chiro1994 <grzegorz.komorowski.1994@gmail.com>
2018-04-15 10:19:01 +0200

    Zadanie 2 - usunięcie metod modelu RequestInfo z WebController

--- validator/src/main/java/com/example/validator/controller/WebController.java
+++ validator/src/main/java/com/example/validator/controller/WebController.java
@@ -2,32 +2,52 @@ package com.example.validator.controller;
 
 import javax.validation.Valid;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.example.validator.model.RequestInfo;
+import com.example.validator.model.ResponseBody;
 
-@Controller
+import static com.example.validator.validators.NipValidator.isNipValid;
+import static com.example.validator.validators.PeselValidator.isPeselValid;
+import static com.example.validator.validators.RegonValidator.isRegonValid;
+import static com.example.validator.validators.EmailValidator.isEmailValid;
+
+import static com.example.validator.validators.PostalCodeValidator.isPostalCodeValid;
+
+@RestController
 public class WebController {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String requestInfo(RequestInfo requestInfo) {
-		return "request";
+	@RequestMapping("/nip")
+	public ResponseBody checkNip(@RequestParam("nip") String nip) {
+		return new ResponseBody(isNipValid(nip));
 	}
-
-	@RequestMapping(value = "/", method = RequestMethod.POST)
-	String requestInfo(@Valid @ModelAttribute("requestInfo") RequestInfo requestInfo, BindingResult bindingResult) {
-
-		if (bindingResult.hasErrors()) {
-			return "request";
-		}
-
-		System.out.println(requestInfo.toString());
-
-		return "successful";
+	
+	@RequestMapping("/pesel")
+	public ResponseBody checkPesel(@RequestParam("pesel") String pesel) {
+		return new ResponseBody(isPeselValid(pesel));
 	}
-
+	
+	@RequestMapping("/regon")
+	public ResponseBody checkRegon(@RequestParam("regon") String regon) {
+		return new ResponseBody(isRegonValid(regon));
+	}
+	
+	@RequestMapping("/postalCode")
+	public ResponseBody checkPostalCode(@RequestParam("postalCode") String postalCode) {
+		return new ResponseBody(isPostalCodeValid(postalCode));
+	}
+	
+	@RequestMapping("/email")
+	public ResponseBody checkEmail(@RequestParam("email") String email){
+		return new ResponseBody(isEmailValid(email));
+	}
+	
 }
